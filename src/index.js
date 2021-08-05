dcf4da4f366d4f5725d5276f2aa165d78d4ef30a
ola <o.bukhal@gmail.com>
2021-08-05 15:11:53 +0300

    feat: add max min avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+
+    let minNumber = array[0];
+    for(el of array) {
+      if (el< minNumber) {
+        minNumber = el;
+      }
+    }
+    return minNumber;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) {
+    return 0;
+  } else {
+    let maxNumber = null;
+    for (el of array) {
+      if (el > maxNumber){
+        maxNumber = el;
+      }
+    }
+    return maxNumber;
+  }
+ 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length ==0 ) {
+    return 0
+  } else {
+    let sum = 0;
+    for (el of array) {
+      sum += el;
+    }
+    return sum/array.length;
+  }
+
 }
